155af64acea7f72819a56a04ef15087d45fed30c
Douglas Burke <dburke.gw@gmail.com>
2023-09-14 11:10:27 -0400

    bump flake; tweak cabal file

--- flake.nix
+++ flake.nix
@@ -31,7 +31,10 @@
             cabal-install
           ];
         # Change the prompt to show that you are in a devShell
-        shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+        shellHook = ''
+  echo -e "*** \e[1;32mWelcome to hsamp\e[0m ***"
+  export PS1='hsamp:\A \e[1;34m\w\e[0m '
+        '';
         });
   };
 }
